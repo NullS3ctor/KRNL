fe667917742b4bfa2f83957c10a73a5949085da5
BillinError <billinerror@gmail.com>
2025-11-26 18:47:35 +0100

    Add files via upload

--- src/include/krnl/mem/stack.h
+++ src/include/krnl/mem/stack.h
@@ -6,19 +6,14 @@
 #include <krnl/libraries/std/stdbool.h>
 typedef struct Stack Stack;
 
-// Creates a new stack
 Stack* stack_create(void);
 
-// Destroys the stack and frees memory
 void stack_destroy(Stack* stack);
 
-// Pushes a new address onto the stack
 void stack_push(Stack* stack, uint64_t address, int regionID);
 
-// Pops and returns the top address from the stack
 uint64_t stack_pop(Stack* stack);
 
-// Checks if the stack is empty
 bool stack_is_empty(Stack* stack);
 
 #endif
